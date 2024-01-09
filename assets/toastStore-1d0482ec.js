da26b5662f364bced2d7bb0bd5c16902a6e4d80d
m_m <minnn7716@gmail.com>
2024-01-09 22:35:27 +0800

    deploy

---
+++ assets/toastStore-1d0482ec.js
@@ -0,0 +1,1 @@
+import{l as a}from"./index-63aa369e.js";const r=a("toastStore",{state:()=>({message:[]}),actions:{pushMsg(t){const{style:s,title:e,content:o}=t;this.message.push({style:s,title:e,content:o})}}});export{r as t};
